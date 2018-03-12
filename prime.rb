0c94afaf7312d421e56ecf72d1a1cf35b33c024a
Parul Gupta <parulguptasmail@gmail.com>
2018-03-12 15:13:49 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,9 @@
 # Add  code here!
 def prime?(number)
-  arr = [2..number-1].to_a 
+  if number >2 
+    arr = [2..number-1].to_a 
+  else
+    arr = [2]
   arr.each do |x|
     if number % x == 0 
       return FALSE
